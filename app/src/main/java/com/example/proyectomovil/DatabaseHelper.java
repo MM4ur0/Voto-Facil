7b8c2855d23232b97e88c1c883e3bc9906b1b8af
Acosta J <69605526+Sprogramdor@users.noreply.github.com>
2023-06-17 19:59:12 -0500

    addd

--- app/src/main/java/com/example/proyectomovil/DatabaseHelper.java
+++ app/src/main/java/com/example/proyectomovil/DatabaseHelper.java
@@ -11,6 +11,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_partido = "CREATE TABLE partidopolitico(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, imagen TEXT, presidente TEXT, fundacion TEXT, posicion TEXT, sede TEXT, pais TEXT);";
     private static final String TABLE_candidato = "CREATE TABLE candidato(id INTEGER PRIMARY KEY AUTOINCREMENT, imagen TEXT, nombre TEXT, cargo TEXT, partidoAfiliado TEXT);";
 
+
+    private static final String TABLE_PAPELETA = "CREATE TABLE Papeleta (id_papeleta INTEGER PRIMARY KEY AUTOINCREMENT, nombre_papeleta TEXT);";
+
+    private static final String TABLE_VOTO = "CREATE TABLE Voto (" +
+            "id_voto INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "usuario_fk INTEGER, " +
+            "papeleta_fk INTEGER, " +
+            "partido_fk INTEGER, " +
+            "FOREIGN KEY(papeleta_fk) REFERENCES Papeleta(id_papeleta) ON DELETE CASCADE, " +
+            "FOREIGN KEY(partido_fk) REFERENCES partido(id_politico) ON DELETE CASCADE, " +
+            "FOREIGN KEY(usuario_fk) REFERENCES Usuario(id_usuario) ON DELETE CASCADE);";
+
+
     /*
     *
     * */
@@ -28,6 +41,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(TABLE_usuario);
         db.execSQL(TABLE_partido);
         db.execSQL(TABLE_candidato);
+        db.execSQL(TABLE_PAPELETA);
+        db.execSQL(TABLE_VOTO);
+
         String insertUsuario = "INSERT INTO usuarios(cedula, nombres, apellidos, genero, edad, correo, contrasena) VALUES('0931760900', 'Mauro Fabrizio', 'Ramos Mesías', 'Masculino', '24', 'framosmesias@gmail.com', '12345')";
         db.execSQL(insertUsuario);
 
@@ -36,6 +52,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         String insertCandidato = "INSERT INTO candidato(imagen, nombre, cargo, partidoAfiliado) VALUES('imagen_candidato.jpg', 'Candidato Y', 'Alcalde', 'Partido X')";
         db.execSQL(insertCandidato);
+
+        String insertPapeleta = "INSERT INTO Papeleta (nombre_papeleta) VALUES ('Papeleta Presidencial')";
+        db.execSQL(insertPapeleta);
+        String insertAlcalde = "INSERT INTO Papeleta (nombre_papeleta) VALUES ('Papeleta de Alcalde')";
+        db.execSQL(insertAlcalde);
+        String insertAsambleistas = "INSERT INTO Papeleta (nombre_papeleta) VALUES ('Papeleta de Asambleistas')";
+        db.execSQL(insertAsambleistas);
+
+
     }
 
     @Override
