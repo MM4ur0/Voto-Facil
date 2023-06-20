72c43e315db7debc223a90709855e3d1db00fca5
MM4ur0 <ramos.maurom@ug.edu.ec>
2023-06-20 02:34:01 -0500

    boton y enviar

--- app/src/main/java/com/example/proyectomovil/ActivityPlan.java
+++ app/src/main/java/com/example/proyectomovil/ActivityPlan.java
@@ -2,11 +2,16 @@ package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ActivityPlan extends AppCompatActivity {
@@ -22,13 +27,23 @@ public class ActivityPlan extends AppCompatActivity {
         String titulo = bundle_temp.getString("titulo");
         plant.setText(titulo);
 
+        ImageButton btn = (ImageButton) findViewById(R.id.btnPdf);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.google.com/";
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
         CheckBox s1 = (CheckBox) findViewById(R.id.s1);
         CheckBox s2 = (CheckBox) findViewById(R.id.s2);
         CheckBox s3 = (CheckBox) findViewById(R.id.s3);
         CheckBox s4 = (CheckBox) findViewById(R.id.s4);
         CheckBox s5 = (CheckBox) findViewById(R.id.s5);
 
-        // Agregar OnCheckedChangeListener para s1
         s1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -40,11 +55,14 @@ public class ActivityPlan extends AppCompatActivity {
                     s5.setButtonDrawable(R.drawable.estrella);
                 } else {
                     s1.setButtonDrawable(R.drawable.estrella);
+                    s2.setButtonDrawable(R.drawable.estrella);
+                    s3.setButtonDrawable(R.drawable.estrella);
+                    s4.setButtonDrawable(R.drawable.estrella);
+                    s5.setButtonDrawable(R.drawable.estrella);
                 }
             }
         });
 
-// Agregar OnCheckedChangeListener para s2
         s2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -57,11 +75,13 @@ public class ActivityPlan extends AppCompatActivity {
                 } else {
                     s1.setButtonDrawable(R.drawable.estrella);
                     s2.setButtonDrawable(R.drawable.estrella);
+                    s3.setButtonDrawable(R.drawable.estrella);
+                    s4.setButtonDrawable(R.drawable.estrella);
+                    s5.setButtonDrawable(R.drawable.estrella);
                 }
             }
         });
 
-// Agregar OnCheckedChangeListener para s3
         s3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -75,11 +95,12 @@ public class ActivityPlan extends AppCompatActivity {
                     s1.setButtonDrawable(R.drawable.estrella);
                     s2.setButtonDrawable(R.drawable.estrella);
                     s3.setButtonDrawable(R.drawable.estrella);
+                    s4.setButtonDrawable(R.drawable.estrella);
+                    s5.setButtonDrawable(R.drawable.estrella);
                 }
             }
         });
 
-// Agregar OnCheckedChangeListener para s4
         s4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -94,11 +115,11 @@ public class ActivityPlan extends AppCompatActivity {
                     s2.setButtonDrawable(R.drawable.estrella);
                     s3.setButtonDrawable(R.drawable.estrella);
                     s4.setButtonDrawable(R.drawable.estrella);
+                    s5.setButtonDrawable(R.drawable.estrella);
                 }
             }
         });
 
-// Agregar OnCheckedChangeListener para s5
         s5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -118,5 +139,7 @@ public class ActivityPlan extends AppCompatActivity {
             }
         });
     }
+
+
 
 }
