d0e3c5d524ef5b7aeefc5f2adbd997b18f3cbc7e
Acosta J <69605526+Sprogramdor@users.noreply.github.com>
2023-06-23 16:08:44 -0500

    update fix ui votoar

--- app/src/main/java/com/example/proyectomovil/DatabaseHelper.java
+++ app/src/main/java/com/example/proyectomovil/DatabaseHelper.java
@@ -1,8 +1,14 @@
 package com.example.proyectomovil;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
+import android.widget.ArrayAdapter;
+
+import com.example.proyectomovil.model.Candidato;
+
+import java.util.ArrayList;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -87,6 +93,37 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String insertAsambleistas = "INSERT INTO Papeleta (nombre_papeleta) VALUES ('Papeleta de Asambleistas')";
         db.execSQL(insertAsambleistas);
     }
+
+    /*public ArrayList<Candidato> mostrarCandidato(){
+        DatabaseHelper hd = new DatabaseHelper(context);
+        SQLiteDatabase db = hd.getWritableDatabase();
+
+        ArrayList<Candidato> list = new ArrayList<>();
+        Candidato cd= null;
+        Cursor c = null;
+
+        c = db.rawQuery("SELECT * FROM " + "candidato", null);
+
+        if(c.moveToFirst()){
+            do{
+                cd= new Candidato();
+                cd.setId(c.getInt(0));
+                cd.setImg(c.getString(1));
+                cd.setNombre(c.getString(2));
+                cd.setCargo(c.getString(3));
+                cd.setIdpartido(c.getInt(4));
+                list.add(cd);
+            }while (c.moveToNext());
+
+            c.close();
+        }
+
+        return list;
+
+    }
+*/
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
---
+++ app/src/main/java/com/example/proyectomovil/model/Candidato.java
@@ -0,0 +1,62 @@
+package com.example.proyectomovil.model;
+
+public class Candidato {
+
+    private  int id ;
+    private  String img;
+    private String nombre;
+    private String cargo;
+    private int idpartido;
+
+
+    public Candidato(int id, String img, String nombre, String cargo, int idpartido) {
+        this.id = id;
+        this.img = img;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.idpartido = idpartido;
+    }
+
+    public Candidato() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getIdpartido() {
+        return idpartido;
+    }
+
+    public void setIdpartido(int idpartido) {
+        this.idpartido = idpartido;
+    }
+}
