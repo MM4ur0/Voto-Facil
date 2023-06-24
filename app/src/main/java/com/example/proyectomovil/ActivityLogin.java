371e6c6663172db984ee71a9e3291543fc853e02
MM4ur0 <ramos.maurom@ug.edu.ec>
2023-06-24 01:13:06 -0500

    Login funcional

--- app/src/main/java/com/example/proyectomovil/ActivityLogin.java
+++ app/src/main/java/com/example/proyectomovil/ActivityLogin.java
@@ -2,15 +2,12 @@ package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -19,99 +16,79 @@ import android.widget.Toast;
 import com.example.proyectomovil.model.Usuario;
 
 public class ActivityLogin extends AppCompatActivity {
-
-
-    Usuario usuario;
+    Usuario usuario = new Usuario();
     EditText txtcedula;
     EditText txtpassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         txtcedula = (EditText) findViewById(R.id.txtcedula);
-        txtpassword=(EditText) findViewById(R.id.txtpassword);
-
+        txtpassword = (EditText) findViewById(R.id.txtpassword);
     }
 
-    public void btnIniciarSesionOnClick(View v){
-        this.getUser();
-       if(usuario.getCedula().equals(txtcedula.getText().toString()) && usuario.getContrasena().equals(txtpassword.getText().toString())){
-            Intent call_registrar = new Intent(this, ActivityInicio.class);
-            startActivity(call_registrar);
-        }else{
-            Toast.makeText(this, "No se encuentra usuario registrado registrado", Toast.LENGTH_SHORT).show();
-        }if(usuario.getCedula().equals(txtcedula.getText().toString()) || usuario.getContrasena().equals(txtpassword.getText().toString())){
-            Toast.makeText(this, "Cedula o Contraseña es incorrecta", Toast.LENGTH_SHORT).show();
+    public void btnIniciarSesionOnClick(View v) {
+
+        if (txtcedula.getText().toString().equals("") && txtpassword.getText().toString().equals("")) {
+            Toast.makeText(this, "No ha ingresado nada", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (txtcedula.getText().toString().equals("")) {
+            Toast.makeText(this, "No ha ingresado la cédula", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (txtpassword.getText().toString().equals("")) {
+            Toast.makeText(this, "No ha ingresado la contraseña", Toast.LENGTH_SHORT).show();
+            return;
         }
 
-
-        Log.d("TAG", usuario.toString());
-
+        if (!txtcedula.getText().toString().equals("") && !txtpassword.getText().toString().equals("")) {
+            this.getUser(txtcedula.getText().toString());
+            if(txtcedula.getText().toString().equals(usuario.getCedula()) && txtpassword.getText().toString().equals(usuario.getContrasena())){
+                Intent call_inicio = new Intent(this, ActivityInicio.class);
+                startActivity(call_inicio);
+                finish();
+            }else{
+                Toast.makeText(this, "Credenciales incorrectas!", Toast.LENGTH_SHORT).show();
+            }
+        }
 
     }
 
-
-    public void getUser() {
+    public void getUser(String identificacion) {
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         final SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Intent call_Inicio = new Intent(this, ActivityInicio.class);
+        try {
+            Cursor c = db.rawQuery("SELECT id, cedula, nombres, apellidos, genero, edad, correo, contrasena, votacion  FROM usuarios WHERE cedula=?" , new String[]{identificacion});
+            if (c != null && c.moveToFirst()) {
+                usuario.setId(c.getInt(0));
+                usuario.setCedula(c.getString(1));
+                usuario.setNombres(c.getString(2));
+                usuario.setApellidos(c.getString(3));
+                usuario.setGenero(c.getString(4));
+                usuario.setEdad(c.getString(5));
+                usuario.setCorreo(c.getString(6));
+                usuario.setContrasena(c.getString(7));
+                usuario.setVotacion(c.getInt(8));
 
-        if (db != null) {
-            String valor = txtcedula.getText().toString();
-            String vclave = txtpassword.getText().toString();
-            Cursor c = null;
-            usuario = new Usuario();
+                SharedPreferences preferencias = getSharedPreferences("usuarioobj", Context.MODE_PRIVATE);
 
-            try {
-                c = db.rawQuery("SELECT id, cedula, nombres, apellidos, genero, edad, correo, contrasena, votacion FROM usuarios WHERE cedula = ? AND contrasena = ?", new String[]{valor, vclave});
-
-                if (c != null && c.moveToFirst()) {
-                    usuario.setId(c.getInt(0));
-                    usuario.setCedula(c.getString(1));
-                    usuario.setNombres(c.getString(2));
-                    usuario.setApellidos(c.getString(3));
-                    usuario.setGenero(c.getString(4));
-                    usuario.setEdad(c.getString(5));
-                    usuario.setCorreo(c.getString(6));
-                    usuario.setContrasena(c.getString(7));
-                    usuario.setVotacion(c.getInt(8));
-
-
-
-                    SharedPreferences preferencias = getSharedPreferences("usuarioobj", Context.MODE_PRIVATE);
-
-                    SharedPreferences.Editor editor = preferencias.edit();
-                    editor.putInt("id", usuario.getId());
-                    editor.commit();
-
-                    startActivity(call_Inicio);
-                } else {
-                    Toast.makeText(this, "No se encuentra registrado", Toast.LENGTH_SHORT).show();
-                }
-            } catch (SQLiteException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Error al consultar la base de datos", Toast.LENGTH_SHORT).show();
-            } finally {
-                if (c != null) {
-                    c.close();
-                }
-                // db.close(); // Cerrar la base de datos si es necesario
+                SharedPreferences.Editor editor = preferencias.edit();
+                editor.putInt("id", usuario.getId());
+                editor.commit();
+            } else {
+                Toast.makeText(this, "No se pudo acceder a la base de datos", Toast.LENGTH_SHORT).show();
             }
-        } else {
-            Toast.makeText(this, "No se pudo acceder a la base de datos", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error rawQuery", Toast.LENGTH_SHORT).show();
         }
     }
 
 
-
-
-
-
-    public void btnRegistrar(View v){
+    public void btnRegistrar(View v) {
         Intent call_registrar = new Intent(this, ActivityRegistroUsuario.class);
         startActivity(call_registrar);
         finish();
     }
-
-
 }
